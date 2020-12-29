13eab0f50f4d0b0f93d4eaaefcc9c9ce6c5abe3b
nekonako <code.yuune@gmail.com>
2020-12-29 18:26:30 +0700

    nganu: adding pwa

--- next.config.js
+++ next.config.js
@@ -13,6 +13,7 @@ module.exports = withReactSvg({
 module.exports = withPWA({
   pwa: {
     disable: process.env.NODE_ENV === 'development',
+    dest: 'public',
     register: true,
     scope: '/',
     sw: 'service-worker.js',
