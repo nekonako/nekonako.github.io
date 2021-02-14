57b87e47ae7b9e6065c7adc7c665beef18dd0948
nekonako <code.yuune@gmail.com>
2021-02-14 22:54:58 +0700

    nganu: adsense

--- pages/_app.js
+++ pages/_app.js
@@ -8,44 +8,48 @@ import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-         <script data-ad-client="ca-pub-7003687861515202" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <meta name='application-name' content='nekonako' />
-        <meta name='apple-mobile-web-app-capable' content='yes' />
-        <meta name='apple-mobile-web-app-status-bar-style' content='default' />
-        <meta name='format-detection' content='telephone=no' />
-        <meta name='mobile-web-app-capable' content='yes' />
-        <meta name='msapplication-config' content='/static/icons/browserconfig.xml' />
-        <meta name='msapplication-TileColor' content='#25293A' />
-        <meta name='msapplication-tap-highlight' content='no' />
-        <meta name='theme-color' content='#25293A' />
+fun
 
-        <link rel='apple-touch-icon' sizes='180x180' href='/assets/icon192.png' />
-        <link rel='icon' type='image/png' sizes='32x32' href='/assets/icon192.png' />
-        <link rel='icon' type='image/png' sizes='16x16' href='/assets/icon192.png' />
-        <link rel='manifest' href='/manifest.json' />
-        <link rel='mask-icon' href='/assets/icon192.png' color='#25293A' />
-        <link rel='shortcut icon' href='/static/icons/safari-pinned-tab.svg' />
-        <meta name='twitter:card' content='Hey! there' />
-        <meta name='twitter:url' content='https://nekonako.me' />
-        <meta name='twitter:title' content='Galih wisnuaji' />
-        <meta name='twitter:description' content='Penikmat kopi dan drama' />
-        <meta name='twitter:image' content='/assets/icon192.png' />
-        <meta name='twitter:creator' content='@Galih_wisnuA' />
-        <meta property='og:type' content='website' /> 
-        <meta property='og:site_name' content='nekonako' />
-        <meta property='og:url' content='https://nekonako.me' />
-             </Head>
-      <ThemeProvider>
-        <div className="flex flex-col h-full min-h-screen font-sans text-base antialiased bg-primary text-primary transition-all duration-300 md:flex-row">
-          <Component {...pageProps} />
-        </div>
-      </ThemeProvider>
-    </>
-  )
+function MyApp({ Component, pageProps }) {
+    return (
+        <>
+            <Head>
+                <script data-ad-client="ca-pub-7003687861515202"
+                    async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js">
+                </script>
+                <meta name='application-name' content='nekonako' />
+                <meta name='apple-mobile-web-app-capable' content='yes' />
+                <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+                <meta name='format-detection' content='telephone=no' />
+                <meta name='mobile-web-app-capable' content='yes' />
+                <meta name='msapplication-config' content='/static/icons/browserconfig.xml' />
+                <meta name='msapplication-TileColor' content='#25293A' />
+                <meta name='msapplication-tap-highlight' content='no' />
+                <meta name='theme-color' content='#25293A' />
+
+                <link rel='apple-touch-icon' sizes='180x180' href='/assets/icon192.png' />
+                <link rel='icon' type='image/png' sizes='32x32' href='/assets/icon192.png' />
+                <link rel='icon' type='image/png' sizes='16x16' href='/assets/icon192.png' />
+                <link rel='manifest' href='/manifest.json' />
+                <link rel='mask-icon' href='/assets/icon192.png' color='#25293A' />
+                <link rel='shortcut icon' href='/static/icons/safari-pinned-tab.svg' />
+                <meta name='twitter:card' content='Hey! there' />
+                <meta name='twitter:url' content='https://nekonako.me' />
+                <meta name='twitter:title' content='Galih wisnuaji' />
+                <meta name='twitter:description' content='Penikmat kopi dan drama' />
+                <meta name='twitter:image' content='/assets/icon192.png' />
+                <meta name='twitter:creator' content='@Galih_wisnuA' />
+                <meta property='og:type' content='website' />
+                <meta property='og:site_name' content='nekonako' />
+                <meta property='og:url' content='https://nekonako.me' />
+            </Head>
+            <ThemeProvider>
+                <div className="flex flex-col h-full min-h-screen font-sans text-base antialiased bg-primary text-primary transition-all duration-300 md:flex-row">
+                    <Component {...pageProps} />
+                </div>
+            </ThemeProvider>
+            </>
+    )
 }
 
 export default MyApp
--- pages/about.js
+++ pages/about.js
@@ -9,78 +9,73 @@ import Masonry from 'react-masonry-css'
 import Link from 'next/link'
 
 const ricing = [
-  {
-    img : '/assets/ricing/nako-classic.png'
-  },
-   {
-    img: '/assets/ricing/furare-girl.png'
-  },
-  {
-    img: '/assets/ricing/fuel.png'
-  },
-   {
-    img: '/assets/ricing/palenight-i3.png'
-  },
+    {
+        img : '/assets/ricing/nako-classic.png'
+    },
+    {
+        img: '/assets/ricing/furare-girl.png'
+    },
+    {
+        img: '/assets/ricing/fuel.png'
+    },
+    {
+        img: '/assets/ricing/palenight-i3.png'
+    },
 ]
 
 const breakpointColumnsObj = {
-  default: 3,
-  1100: 3,
-  700: 2,
-  500: 1
+    default: 3,
+    1100: 3,
+    700: 2,
+    500: 1
 };
 
 function About() {
-  return (
-    <>
-      <Head>
-        <title>About | Nekonako</title>
-        <meta data-hid="og:image" property="og:image" content="/assets/nako.jpg"/>
-        <meta property='og:image' content="/assets/nako.jpg" />
-      </Head>
-      <Navbar />
-      <SideNav/>
-      <div className="relative flex w-full min-h-screen md:w-2/3">
-        <main className="flex-1">
-          <div className="min-h-screen px-4 py-1 mt-20 md:m-8">
-            <div className="text-2xl font-bold"> <span className="text-accent"># </span>
-              Hallo Semuanya</div> Nama saya Galih Wisnuaji.
-            <br/>
-            Cuma seorang penikmat kopi dan drama.
-            <div className="pt-8 text-2xl font-bold">
-              <span className="text-accent"># </span>
-              Kontak</div>
-            <a href="mailto:code.yuune@gmail.com" rel="external noopener">ne.nekonako@gmail.com</a>
-
-            <div className='mt-8 footer'></div>
-
-            <div className='py-4 text-2xl font-bold'>Ricing Journey</div>
-            <Masonry
-              breakpointCols={breakpointColumnsObj}
-              className="my-masonry-grid"
-              columnClassName="my-masonry-grid_column"
-            >
-              {ricing.map((ricing)=> {
-                return(
-                  <>
-
-                    <Link href={ricing.img}>
-                      <div className='overflow-hidden shadow-lg rounded-md'>
-                        <img src={ricing.img}/>
-                      </div>
-                    </Link>
-                  </>
-                )
-              })}
-            </Masonry>
-
-
-          </div>
-          <Footer />
-        </main>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <Head>
+                <title>About | Nekonako</title>
+                <meta data-hid="og:image" property="og:image" content="/assets/nako.jpg"/>
+                <meta property='og:image' content="/assets/nako.jpg" />
+            </Head>
+            <Navbar />
+            <SideNav/>
+            <div className="relative flex w-full min-h-screen md:w-2/3">
+                <main className="flex-1">
+                    <div className="min-h-screen px-4 py-1 mt-20 md:m-8">
+                        <div className="text-2xl font-bold"> <span className="text-accent"># </span>
+                        Hallo Semuanya</div> Nama saya Galih Wisnuaji.
+                        <br/>
+                        Cuma seorang penikmat kopi dan drama.
+                        <div className="pt-8 text-2xl font-bold">
+                            <span className="text-accent"># </span>
+                        Kontak</div>
+                        <a href="mailto:code.yuune@gmail.com" rel="external noopener">ne.nekonako@gmail.com</a>
+                        <div className='mt-8 footer'></div>
+                        <div className='py-4 text-2xl font-bold'>Ricing Journey</div>
+                        <Masonry
+                            breakpointCols={breakpointColumnsObj}
+                            className="my-masonry-grid"
+                            columnClassName="my-masonry-grid_column"
+                        >
+                            {ricing.map((ricing)=> {
+                                return(
+                                    <>
+                                        <Link href={ricing.img}>
+                                            <div className='overflow-hidden shadow-lg rounded-md'>
+                                                <img src={ricing.img}/>
+                                            </div>
+                                        </Link>
+                                        </>
+                                )
+                            })}
+                        </Masonry>
+                    </div>
+                    <Footer />
+                </main>
+            </div>
+            </>
+    )
 }
 
 export default About
