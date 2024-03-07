bb96888474bdc6c2ab0b745df0a86f3206dabe52
nekonako <nekonako@users.noreply.github.com>
2024-03-07 11:56:25 +0000

    deploy: 0a01079c7e88106e4d608f0350850fe15b967e57

---
+++ _next/static/EGCOHObFR7GzjqULJMZhB/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fprojects","\u002Fposts","\u002Fproject\u002F[slug]","\u002Fpost\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- _next/static/HybwacQOHIYuMU71eepTZ/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fposts","\u002Fprojects","\u002Fpost\u002F[slug]","\u002Fproject\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
